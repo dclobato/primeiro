f705baa11a5a4438111f6d905505235ff6c9f3f9
Daniel Correa Lobato <dclobato@users.noreply.github.com>
2016-05-17 21:48:21 -0300

    Commit para resolver o issue 1

--- caraecoroa.c
+++ caraecoroa.c
@@ -6,11 +6,10 @@ int main()
   int maria, joao;
   int jogadas, i, jogada;
   
-  caras = 0;
-  coroas = 0;
-  scanf("%d", &jogada);
-  while (jogada != 0)
+  while (jogadas != 0)
   {
+    caras = 0;
+    coroas = 0;
     for (i = 0; i < jogadas; i++)
     {
       scanf("%d", &jogada);
@@ -18,5 +17,6 @@ int main()
       else             joao++;
     }
     printf("Maria ganhou %d vezes e Joao ganhou %d vezes\n", maria, joao);
+    scanf("%d", &jogadas);
   }
 }
